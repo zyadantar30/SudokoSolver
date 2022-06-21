622fcb77d73f1a0e7196aba8caa1f3a55c956508
zyadantar30 <49242363+zyadantar30@users.noreply.github.com>
2022-06-21 16:41:20 +0200

    Add files via upload

---
+++ sudokoSolver.py
@@ -0,0 +1,168 @@
+"""
+Created on Tue Jun 21 2022
+
+@author: Zyad Antar
+"""
+
+import pprint
+
+
+def solve(bo):
+    """
+    Parameters
+    ----------
+    bo : 2d list of integers
+        Take the board and solve it using backtracking algorithm.
+
+    solution to the board
+    -------
+    """
+    find = find_empty(bo)
+    if find:
+        row , col = find
+    else:
+        return True
+    
+    for i in range(1 , 10):
+        if valid(bo , (row , col) , i):
+            bo[row][col] = i
+            
+            if solve(bo):
+                return True
+            
+            bo[row][col] = 0
+            
+    return False
+
+
+def valid(bo , pos ,num):
+    """
+    Parameters
+    ----------
+    bo : 2d list of int
+    pos : (row , col).
+    num : int.
+
+    Returns Boolean
+    -------
+    """
+    
+    #check row
+    for i in range(0 , len(bo)):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+        
+    #check col 
+    for i in range(0 , len(bo)):
+        if bo[i][pos[1]] == num and pos[1] != i:
+            return False
+        
+    #check box 
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    
+    for i in range(box_y*3 , box_y*3 + 3):
+        for j in range(box_x*3 , box_x*3 + 3):
+            if bo[i][j] == num and (i , j) != pos:
+                return False
+            
+    return True
+
+
+def find_empty(bo):
+    """
+    Parameters
+    finds an empty space in the board
+    ----------
+    bo : 2d list of int.
+
+    Returns (int , int) --> row , col 
+    -------
+    """
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i , j)
+    
+    return None
+
+
+def print_board(bo):
+    """
+    print the board
+    Parameters
+    ----------
+    bo : 2d list of integers
+
+    Returns None
+    -------
+    """
+                
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - -")
+        for j in range(len(bo[0])):
+            if j % 3 == 0:
+                print(" | ",end="")
+
+            if j == 8:
+                print(bo[i][j], end="\n")
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+board = [
+        [7 , 8 , 0 , 4 , 0 , 0 , 1 , 2 , 0],
+        [6 , 0 , 0 , 0 , 7 , 5 , 0 , 0 , 9],    
+        [0 , 0 , 0 , 6 , 0 , 1 , 0 , 7 , 8],
+        [0 , 0 , 7 , 0 , 4 , 0 , 2 , 6 , 0],
+        [0 , 0 , 1 , 0 , 5 , 0 , 9 , 3 , 0],
+        [9 , 0 , 4 , 0 , 6 , 0 , 0 , 0 , 5],
+        [0 , 7 , 0 , 3 , 0 , 0 , 0 , 1 , 2],
+        [1 , 2 , 0 , 0 , 0 , 7 , 4 , 0 , 0],
+        [0 , 4 , 9 , 2 , 0 , 6 , 0 , 0 , 7],
+    ]
+
+pp = pprint.PrettyPrinter(width = 41 , compact=True)
+solve(board)
+pp.pprint(board)
+ 
+        
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
